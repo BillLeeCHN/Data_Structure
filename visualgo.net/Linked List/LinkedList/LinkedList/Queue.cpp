119a694afad54651096e375bb69752cc2fd9fe69
BillLeeCHN <785348071@qq.com>
2017-02-12 18:15:58 +0800

    Enqueue funtion

--- visualgo.net/Linked List/LinkedList/LinkedList/Queue.cpp
+++ visualgo.net/Linked List/LinkedList/LinkedList/Queue.cpp
@@ -14,16 +14,33 @@ typedef struct _queue
 }Queue;
 
 void Create(Queue* pq);
+bool IsEmpty(Queue* pq);
+void Enqueue(Queue* pq, int value);
 
 int main()
 {
 	Queue q;
-
+	// Create
 	Create(&q);
+	//enqueue
+	Enqueue(&q, 10);
+	// empty?
+	if (IsEmpty(&q))
+	{
+		printf("The queue is empty.\n");
+	}
+	else
+	{
+		printf("The queue is not empty.\n");
+	}
 
+	
+	
+	
+	
 }
 
-//
+// Create a new queue
 void Create(Queue* pq)
 {
 	// Create a new node
@@ -37,5 +54,36 @@ void Create(Queue* pq)
 
 	//
 	pq->front = pnew;
+	pq->rear = pnew;
+}
+
+// Is the queue empty?
+bool IsEmpty(Queue* pq)
+{
+	if (pq->front == pq->rear)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+// put element into the queue
+void Enqueue(Queue* pq, int value)
+{
+	// Create a new node
+	node* pnew = (node*)malloc(sizeof(node));
+	if (NULL == pnew)
+	{
+		printf("create failed!\n");
+		return;
+	}
+	pnew->next = NULL;
+	pnew->data = value;
+
+	// enqueue
+	pq->rear->next = pnew;
 	pq->rear = pnew;
 }
