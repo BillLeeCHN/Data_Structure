c1b92893e1f19e2e2c8ea566f2684a9d9de34efa
BillLeeCHN <785348071@qq.com>
2017-02-12 14:46:20 +0800

    add function

--- visualgo.net/Linked List/LinkedList/LinkedList/LinkedList.cpp
+++ visualgo.net/Linked List/LinkedList/LinkedList/LinkedList.cpp
@@ -8,10 +8,15 @@ typedef struct _LList
 }LList;
 
 LList* Create();
+void Add(LList* head, int value);
 
 int main()
 {
 	LList* p = Create();
+	Add(p, 10);
+	Add(p, 20);
+	Add(p, 30);
+	Add(p, 40);
 	
 	return 0;
 }
@@ -19,10 +24,10 @@ int main()
 // 创建一个空的链表，仅有头结点和头指针
 LList* Create()
 {
-	LList* head; // 头指针
+	LList* head; // head pointer
 	head = (LList*)malloc(sizeof(LList));
 
-	//创建失败返回
+	//create failed
 	if (head==NULL) 
 	{ 
         printf("创建链表失败！");
@@ -31,4 +36,32 @@ LList* Create()
 
 	head->next = NULL;
 	return head;
+}
+
+// 向链表尾部添加元素
+void Add(LList* head, int value)
+{
+	LList* ptemp = head;
+
+	// if ptemp does not point to the tail node,
+	// move ptemp to the next node until to the tail node
+	while(ptemp->next != NULL)
+	{
+		ptemp = ptemp->next;
+	}
+
+	// Create a new node
+	LList* pnew;
+	pnew = (LList*)malloc(sizeof(LList));
+	//创建失败返回
+	if (pnew==NULL) 
+	{ 
+        printf("创建链表失败！");
+        return;
+    }
+	pnew->next = NULL;
+	pnew->data = value; // assign a value
+
+	// Add the new node
+	ptemp->next = pnew;
 }
