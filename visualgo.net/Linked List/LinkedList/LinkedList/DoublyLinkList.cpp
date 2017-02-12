19da1821c32e976992022fac65237e9496140edf
BillLeeCHN <785348071@qq.com>
2017-02-12 20:57:36 +0800

    Add function

--- visualgo.net/Linked List/LinkedList/LinkedList/DoublyLinkList.cpp
+++ visualgo.net/Linked List/LinkedList/LinkedList/DoublyLinkList.cpp
@@ -15,11 +15,14 @@ typedef struct _dbList
 }dbList;
 
 void Create(dbList* pdl);
+void Add(dbList* pdl, int value);
 
 int main()
 {
 	dbList dl;
 	Create(&dl);
+	// Add
+	Add(&dl, 10);
 
 
 	return 0;
@@ -39,5 +42,24 @@ void Create(dbList* pdl)
 	pnew->next = NULL;
 	//
 	pdl->head = pnew;
+	pdl->tail = pnew;
+}
+
+// Add element to the tail of double linked list
+void Add(dbList* pdl, int value)
+{
+	// Create a node
+	dbNode* pnew = (dbNode*)malloc(sizeof(_dbNode));
+	if (NULL == pnew)
+	{
+		printf("Create failed.\n");
+		return;
+	}
+	pnew->prev = NULL;
+	pnew->next = NULL;
+	pnew->data = value;
+	//Add node to the tail
+	pdl->tail->next = pnew;
+	pnew->prev = pdl->tail;
 	pdl->tail = pnew;
 }
